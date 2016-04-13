41b663654e540d88336525c6addeb9678a3d635c
Scott Holland <scotthollandjr@gmail.com>
2016-04-12 17:23:35 -0700

    Attempt user interface (unfinished)

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,50 @@
 // business logic
+function Tickets(tickets) {
+  this.tickets = [];
+}
+
 function Ticket(age, time, movie) {
   this.age = age;
   this.time = time;
   this.movie = movie;
 }
 
+Ticket.prototype.ticketDetails = function() {
+  return this.age + " at " + this.time  + ". Cost: " + this.ticketPrice;
+  }
+
 // user interface logic
 $(document).ready(function() {
   $("form#ticket-pricer").submit(function(event) {
     event.preventDefault();
 
-    var ageInput = $("input[name=radAge]:radio:checked").val();
-    var timeInput = $("#timeSelect").val();
-    var movieInput = $("#movieSelect").val();
-    var movieSelect = new Ticket(ageInput, timeInput, movieInput);
 
-    var ticketPrice = 6;
-    var ticketCalculator = function() {
-      if (( ageInput != 65 ) && ( ageInput != 12 )) {
-        ticketPrice += 2;
+
+    $(".add-tickets").each(function() {
+      var ageInput = $("input[name=radAge]:radio:checked").val();
+      var timeInput = $("#timeSelect").val();
+      var movieInput = $("#movieSelect").val();
+
+      var movieSelect = new Ticket(ageInput, timeInput, movieInput);
+      var ticketPrice = 6;
+
+      var ticketCalculator = function() {
+        if (( ageInput != 65 ) && ( ageInput != 12 )) {
+          ticketPrice += 2;
+        }
+        if ( timeInput > 600 ) {
+          ticketPrice += 2;
+        }
+        if ( movieInput === "newRelease" ) {
+          ticketPrice += 1;
+        }
       }
-      if ( timeInput > 600 ) {
-        ticketPrice += 2;
-      }
-      if ( movieInput === "newRelease" ) {
-        ticketPrice += 1;
-      }
-    }
-    ticketCalculator();
-    alert(ticketPrice);
+      ticketCalculator();
+    });
+
+
+
+
   });
+  $("ul#ticket-display").append("<li>" + ticketDetails() + "</li>")
 });
